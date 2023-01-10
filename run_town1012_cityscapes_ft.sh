83f8ba73cb53ff9f3745335131df375b8c064295
George Tian <tianbeiwen88@gmail.com>
2023-01-11 03:44:36 +0800

    update scripts

---
+++ run_town1012_cityscapes_ft.sh
@@ -0,0 +1,11 @@
+python main.py --model deeplabv3plus_resnet101\
+       	--dataset cityscapes\
+       	--lr 0.1\
+       	--crop_size 768\
+       	--batch_size 16\
+       	--output_stride 16\
+       	--data_root /data3/tb5zhh/workspace/datasets/cityscapes\
+       	--gpu_id 3,6\
+       	--run_name "CARLAT10&12_FT_Cityscapes_0.1"\
+       	--ckpt checkpoints/carla12-carla10_best_0.842460_iter28600_deeplabv3plus_resnet101_carla_os16.pth \
+       	--wandb
